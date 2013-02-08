f53b75d477dac09073e0576cb57bf94a8620b252
Bernardo Tuso <btuso.95@gmail.com>
2013-02-08 07:28:23 -0300

    Stats updated to new path

--- js/GMN/Stats/getStats.js
+++ js/GMN/Stats/getStats.js
@@ -1,6 +1,6 @@
 GMN.Stats.getStats = function(callback) {
 		$.ajax({
-			"url": GMN.Server.Config.getServer() + ":" + GMN.Server.Config.getPort() + "/stats",
+			"url": GMN.Server.Config.getServer() + ":" + GMN.Server.Config.getPort() + "/admin/stats/" + GMN.Server.Config.getAdminPassword(),
 			"complete": function(data) {
 				var msg = data.responseText ? JSON.parse(data.responseText) : {error:'0', message:'unhandled error'};
 				callback(msg,data.status);
